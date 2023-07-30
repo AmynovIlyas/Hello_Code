bbabfa06978854ce379005e2d11b5ee49fcd734f
Ilyas Aminov <aminov.i.g@yandex.ru>
2023-07-31 00:28:36 +0300

    i understand solution of the task example007

--- Example007_Cycle/Program.cs
+++ Example007_Cycle/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.Clear();
-int xa = 40, ya = 1,
-    xb = 1, yb = 20,
+int xa = 40, ya = 0,
+    xb = 0, yb = 20,
     xc = 80, yc = 20;
 Console.SetCursorPosition(xa, ya);
 Console.WriteLine("+");
@@ -11,32 +11,29 @@ Console.WriteLine("+");
 Console.SetCursorPosition(xc, yc);
 Console.WriteLine("+");
 
-// int x = xa, y = xb;
+int x = xa, y = ya;
 
-// int count = 0;
+int count = 0;
 
-// while (count < 1)
-// {
-//     int what = new Random().Next(0, 3); // [0;3) 0, 1, 2
-//     if (what == 0)
-//     {
-//         x = (x + xa) / 2;
-//         y = (y + ya) / 2;
-//     }
-
-//     if (what == 1)
-//     {
-//         x = (x + xb) / 2;
-//         y = (y + yb) / 2;
-//     }
-
-//     if (what == 2)
-//     {
-//         x = (x + xc) / 2;
-//         y = (y + yc) / 2;
-//     }
-
-//     Console.SetCursorPosition(x, y);
-//     Console.WriteLine("+");
-//     count++;
-// }
+while (count < 10000)
+{
+    int what = new Random().Next(0, 3); // [0;3) 0, 1, 2
+    if (what == 0)
+    {
+        x = (x + xa) / 2;
+        y = (y + ya) / 2;
+    }
+    if (what == 1)
+    {
+        x = (x + xb) / 2;
+        y = (y + yb) / 2;
+    }
+    if (what == 2)
+    {
+        x = (x + xc) / 2;
+        y = (y + yc) / 2;
+    }
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+    count++;
+}
